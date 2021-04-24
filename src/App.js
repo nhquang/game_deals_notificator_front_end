f1148c32decad39c2e42d41c4a19ec5f8ba3065e
nhquang <quangpersie1@gmail.com>
2021-04-24 12:49:16 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+import { useState, useEffect } from 'react'
+
+function App() {
+  return (
+    <div className="container">
+      <Header title = "Game Price Notificator"/>
+
+    </div>
+  );
+}
+
+export default App;
