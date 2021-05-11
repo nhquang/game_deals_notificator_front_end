709e2be260ceff03cdd8547e98f31dd4a9160985
nhquang <quangpersie1@gmail.com>
2021-05-10 22:15:37 -0400

    more precise currency exchange

--- src/components/Deal.js
+++ src/components/Deal.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
 function Deal({storeName, storeURL, price}) {
+    const rate1 = 1.3334;
+    const rate2 = 1.1403;
+    const rate3 = 1.1154;
+    let cprice = 0;
+    if(price > 18.5) cprice = price * rate1;
+    else if (price > 9) cprice = price * rate2;
+    else cprice = price * rate3;
     return (
         <div className="deal">
             <h3><a href={storeURL} style={{textDecoration:"none"}} target="_blank" rel="noreferrer">{storeName}</a></h3>
-            Price: ${price}
+            Price: ${price} ~ C${Math.round((cprice + Number.EPSILON) * 100) / 100}
         </div>
     )
 }
--- src/components/Deals.js
+++ src/components/Deals.js
@@ -5,7 +5,7 @@ function Deals(props) {
     return (
         <div className={`${ props.deals.length > 2 ? "deals" : ""}`} style={{ padding: "0px 20px 20px 20px", marginBottom:"20px", height: props.deals.length > 2 ? "200px" : "auto", overflow: props.deals.length > 2 ? "auto" : "hidden"}}>
             <h3 style={{textAlign:"center"}}>Current Prices</h3>
-            <p style={{fontSize:"13px", textAlign:"center", marginBottom:"10px"}}>* All the prices are in US dollar</p>
+            <p style={{fontSize:"13px", textAlign:"center", marginBottom:"10px"}}>* US dollar ~ Canadian dollar</p>
             {props.deals.map((item) =>(
                 <Deal key = {item.dealID} storeName = {item.store} storeURL = {item.storeURL} price = {item.price}/>
             ))}
