3567f090160efab5e6fcf65eff7c3e3e8cb5ebb5
nhquang <quangpersie1@gmail.com>
2021-05-15 14:51:40 -0400

    display price alert currency

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -5,7 +5,7 @@ function Alert(props) {
     return (
         <div className="item" style={{cursor: "auto"}}>
             <h3>{props.game} <FaTimes onClick={() => props.onDelete(props.gameid, props.price, props.email, props.name, props.currency, props.game)} style={{color:"red", cursor:"pointer"}}/></h3>
-            <p>Price: ${props.price}</p>
+            <p>Price: {props.currency == 2 && "C"}${props.price}</p>
         </div>
     )
 }
